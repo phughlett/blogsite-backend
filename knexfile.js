3f2c9c6d8df64d1d5d8a0f0469bf02bdbafcf6bf
Patrick Hughlett <phughlett87@gmail.com>
2022-03-22 18:48:31 -0600

    adjusted production string

--- knexfile.js
+++ knexfile.js
@@ -27,19 +27,9 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    client: 'pg',
+    connection: process.env.CONNECTION_STRING
     }
-  }
+  };
 
-};
+
