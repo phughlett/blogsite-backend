a3fd4b9a60d9b37058a6f3f045a6b60ddff7bf08
Patrick Hughlett <phughlett87@gmail.com>
2022-03-22 18:59:05 -0600

    adjusted production string

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,7 @@
 // Update with your config settings.
 require('dotenv').config();
+
+let connectionString = process.env.DATABASE_URL;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
@@ -28,7 +30,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {process.env.CONNECTION_STRING, ssl:{rejectUnauthorized: false}}
+    connection: {connectionString, ssl:{rejectUnauthorized: false}}
     }
   };
 
